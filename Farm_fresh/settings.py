8f370d5bf7e92a313c2b9e6030c3b320068ab723
Akash Karthi <akashkarthiviswanathan@gmail.com>
2021-11-28 13:37:17 +0530

    commit

--- Farm_fresh/settings.py
+++ Farm_fresh/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -134,3 +134,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 #Activate django-heroku
 django_heroku.settings(locals())
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
